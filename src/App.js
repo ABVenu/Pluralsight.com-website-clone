0d757285c1a0fe71f8fa00c172898a2921e1e8e1
ABVenu <101357178+ABVenu@users.noreply.github.com>
2022-07-21 01:41:49 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import Footer from './compoenents/Footer';
+import Navbar from './compoenents/Navbar';
+import Nav from './compoenents/ex';
+
+function App() {
+  return (
+    <div className="App">
+      {/* <Footer /> */}
+      {/* <Navbar /> */}
+      <Nav />
+    </div>
+  );
+}
+
+export default App;
