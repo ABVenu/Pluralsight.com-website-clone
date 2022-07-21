a3b4a6ff35a6440db670f331674d72e2365596cc
ABVenu <101357178+ABVenu@users.noreply.github.com>
2022-07-22 02:15:33 +0530

    Day-3 work

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+import Footer from './compoenents/Footer';
+import Navbar from './compoenents/Navbar';
+import Nav from './compoenents/Nav';
+import Body from './compoenents/Body';
+
+function App() {
+  return (
+    <div className="App">
+      {/* <Footer /> */}
+      {/* <Navbar /> */}
+      {/* <Nav /> */}
+      <Body />
+    </div>
+  );
+}
+
+export default App;
